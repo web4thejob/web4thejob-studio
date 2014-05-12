f25d2c1363929ff03e7fa4382fb6ea5b5c5aa66f
bissaias <bissaias@hotmail.com>
2014-05-12 16:01:59 +0300

    infrastructure steps

--- web4thejob-studio/src/main/webapp/js/designer.js
+++ web4thejob-studio/src/main/webapp/js/designer.js
@@ -19,6 +19,9 @@ var w4tjStudioDesigner = {
         this._designer=zk('$designer').$();
         this._desktopId=zk.Desktop.$().id;
         this.makeTemplatesDraggable();
+
+        //bs tooltip example
+        jq("#nav-top-navbar a").attr("data-toggle","tooltip").attr("data-placement","bottom").tooltip();
     },
 
     makeTemplatesDraggable: function() {
@@ -29,6 +32,25 @@ var w4tjStudioDesigner = {
         .on('dragend',function(e){
             e.preventDefault()
          });
+    },
+
+
+    alert: function(clazz,title,message){
+        jq('body').css('overflow','hidden');
+        var a='<div style="white-space:nowrap;position:absolute;top:70%;left:'+jq(window).width()+'px;z-index:10000;min-width:200px" class="alert '+clazz+' alert-dismissable"><button type="button" class="close" aria-hidden="true">&times;</button><strong>'+title+'</strong> '+message+'.</div>';
+        jq('body').append(a);
+        jq('.alert .close').click( function () {
+          jq( ".alert" ).fadeOut( "slow",function(){jq( ".alert" ).remove();});
+        });
+        jq('.alert').animate({left:'40%'},1000);
+    },
+
+    //inter-frame communication
+    sendToCanvas: function(name,data) {
+        frames[zk("$canvasHolder").$().uuid].w4tjStudioCanvas.sendEvent(name,data);
+    },
+    sendEvent: function(name,data){
+        zAu.send(new zk.Event(zk("$designer").$(), name, data));
     }
 
 
