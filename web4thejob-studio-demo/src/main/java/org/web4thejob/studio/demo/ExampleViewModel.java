1ebd9a7161766582d6558da59484ebb1761061c5
Veniamin Isaias <bissaias@hotmail.com>
2014-09-18 11:18:05 +0300

    mvvm improvements

--- web4thejob-studio-demo/src/main/java/org/web4thejob/studio/demo/ExampleViewModel.java
+++ web4thejob-studio-demo/src/main/java/org/web4thejob/studio/demo/ExampleViewModel.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2014 Veniamin Isaias
+ *
+ * This file is part of Web4thejob Studio.
+ *
+ * Web4thejob Studio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Web4thejob Studio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Web4thejob Studio.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.web4thejob.studio.demo;
 
 import com.web4thejob.jpatest.Customer;
