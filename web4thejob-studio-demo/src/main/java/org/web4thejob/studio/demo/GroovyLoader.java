336edbfa13346e32f6c7c3cc8a6bc7cd980c1e1e
Veniamin Isaias <bissaias@hotmail.com>
2014-11-13 21:21:05 +0200

    groovy composers

--- web4thejob-studio-demo/src/main/java/org/web4thejob/studio/demo/GroovyLoader.java
+++ web4thejob-studio-demo/src/main/java/org/web4thejob/studio/demo/GroovyLoader.java
@@ -1,6 +1,7 @@
 package org.web4thejob.studio.demo;
 
 import groovy.lang.GroovyClassLoader;
+import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
@@ -14,9 +15,38 @@ public class GroovyLoader extends SelectorComposer<Window> {
     @Wire
     private Textbox txtSource;
 
+    @Override
+    public void doAfterCompose(Window comp) throws Exception {
+        super.doAfterCompose(comp);
+        String src = (String) Executions.getCurrent().getDesktop().getWebApp().getAttribute("MyGroovyComposer");
+        if (src == null) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("package foo.bar").append("\n");
+            builder.append("import org.zkoss.zk.ui.select.SelectorComposer").append("\n");
+            builder.append("import org.zkoss.zk.ui.Component").append("\n");
+            builder.append("import org.zkoss.zk.ui.select.annotation.Wire").append("\n");
+            builder.append("import org.zkoss.zul.Label").append("\n");
+            builder.append("\n");
+            builder.append("class MyGroovyComposer extends SelectorComposer<Component> {").append("\n");
+            builder.append("	@Wire").append("\n");
+            builder.append("	Label lblHello").append("\n");
+            builder.append("\n");
+            builder.append("	public void doAfterCompose(Component comp) {").append("\n");
+            builder.append("		super.doAfterCompose(comp);").append("\n");
+            builder.append("		lblHello.setValue(this.class.name)").append("\n");
+            builder.append("	}").append("\n");
+            builder.append("}").append("\n");
+            src = builder.toString();
+        }
+
+        txtSource.setValue(src);
+        onCompile();
+    }
+
     @Listen("onClick=#btnCompile")
-    public void onCompile() {
+    public synchronized void onCompile() {
         GroovyClassLoader loader = GroovyInitializer.GROOVY_LOADER;
         loader.parseClass(txtSource.getValue());
+        Executions.getCurrent().getDesktop().getWebApp().setAttribute("MyGroovyComposer", txtSource.getValue());
     }
 }
